6ed4dbb6807b841c187552a1a1ccd35ebd979265
Niels Sandholt Busch <niels.busch@gmail.com>
2016-09-17 18:15:47 +0200

    setup elm material design

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require( '../../node_modules/material-design-lite/material.css' );
+require( '../../node_modules/dialog-polyfill/dialog-polyfill.js' );
 
 var Elm = require('../elm/Main.elm');
 var mountNode = document.getElementById('main');
