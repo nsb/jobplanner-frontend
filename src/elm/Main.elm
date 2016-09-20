c8ffb7edd59d36a04a4b87a67b64dcc414881240
Niels Sandholt Busch <niels.busch@gmail.com>
2016-09-20 22:56:21 +0200

    more material design

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -7,25 +7,22 @@ import Material
 import Material.Layout as Layout
 import Material.Color exposing (Hue(..))
 import Material.Scheme as Scheme
+import Material.Grid exposing (grid, cell, size, offset, Device(..))
 
 
 -- MODEL
 
 
 type alias Model =
-    { count : Int
-    , mdl :
+    { mdl :
         Material.Model
-        -- Boilerplate: model store for any and all Mdl components you use.
     }
 
 
 model : Model
 model =
-    { count = 0
-    , mdl =
+    { mdl =
         Material.model
-        -- Boilerplate: Always use this initial Mdl model store.
     }
 
 
@@ -34,29 +31,13 @@ model =
 
 
 type Msg
-    = Increase
-    | Reset
-    | Mdl (Material.Msg Msg)
+    = Mdl (Material.Msg Msg)
 
-
-
--- Boilerplate: Msg clause for internal Mdl messages.
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increase ->
-            ( { model | count = model.count + 1 }
-            , Cmd.none
-            )
-
-        Reset ->
-            ( { model | count = 0 }
-            , Cmd.none
-            )
-
-        -- Boilerplate: Mdl action handler.
         Mdl msg' ->
             Material.update msg' model
 
@@ -69,16 +50,46 @@ type alias Mdl =
     Material.Model
 
 
+drawer : List (Html Msg)
+drawer =
+  [ Layout.title [] [ text "JobPlanner" ]
+  , Layout.navigation
+    []
+    [  Layout.link
+        [ Layout.href "/calendar" ]
+        [ text "Calendar" ]
+    , Layout.link
+        [ Layout.href "/clients" ]
+        [ text "Clients" ]
+    , Layout.link
+        [ Layout.href "/work"
+        , Layout.onClick (Layout.toggleDrawer Mdl)
+        ]
+        [ text "Work" ]
+    ]
+  ]
+
+
 header : Html Msg
 header =
     Layout.row []
         [ Layout.title [] [ text "JobPlanner" ]
         , Layout.spacer
         , Layout.navigation []
-            [ Layout.link [ Layout.href "https://elm-lang.org/" ]
-                [ text "elm-lang.org" ]
+            [ Layout.link [ Layout.href "https://www.jobplanner.dk" ]
+                [ text "JobPlanner" ]
             ]
         ]
+
+
+content : (Html a)
+content =
+  grid []
+    [ cell [ size Tablet 8, size Desktop 12, size Phone 4 ]
+        [ h4 [] [text "Cell 4"]
+        , p [] [text "Size varies with device"]
+        ]
+    ]
 
 
 view : Model -> Html Msg
@@ -93,10 +104,10 @@ view model =
             , Layout.waterfall True
             ]
             { header = [ header ]
-            , drawer = [ text "davs" ]
+            , drawer = drawer
             , tabs = ( [], [] )
             , main =
-                [ Html.text "hejsa"
+                [ content
                 ]
             }
         ]
